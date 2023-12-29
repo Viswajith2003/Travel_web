ed3b7fa2c6ac97ee178b9590e0a4d81e68f6c405
Viswajith2003 <viswajithviswa715@gmail.com>
2023-12-30 01:57:45 +0530

    footer completed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,32 +2,33 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="flex justify-between md:px-32 px-5 gap-10">
-      <div>
-        <h1>TripMood</h1>
-        <p className="text-left">
-          With TripMood you can experience <br /> new travel and the best tourist <br />
+    <div className="mb-14 mt-8 flex justify-between md:px-32 px-5 gap-10">
+      <div className="space-y-5">
+        <h1 className="text-3xl font-bold">TripMood</h1>
+        <p className="font-semibold text-gray-600">
+          With TripMood you can experience <br /> new travel and the best
+          tourist <br />
           destination that we have to offer
         </p>
       </div>
-      <div>
-        <h1>Destination</h1>
-        <h3>Paris,France</h3>
-        <h3>Dubai,UAE</h3>
-        <h3>Venice, Italy</h3>
-        <h3>Lakshadweep</h3>
+      <div className="space-y-4">
+        <h1 className="text-2xl font-bold">Destination</h1>
+        <h3 className="font-semibold text-gray-600">Paris,France</h3>
+        <h3 className="font-semibold text-gray-600">Dubai,UAE</h3>
+        <h3 className="font-semibold text-gray-600">Venice, Italy</h3>
+        <h3 className="font-semibold text-gray-600">Lakshadweep</h3>
       </div>
-      <div>
-        <h1>About</h1>
-        <h3>Contact us</h3>
-        <h3>Testimonial</h3>
-        <h3>Rating</h3>
+      <div className="space-y-4">
+        <h1 className="text-2xl font-bold">About</h1>
+        <h3 className="font-semibold text-gray-600">Contact us</h3>
+        <h3 className="font-semibold text-gray-600">Testimonial</h3>
+        <h3 className="font-semibold text-gray-600">Rating</h3>
       </div>
-      <div>
-        <h1>Follow Us</h1>
-        <p>facebook</p>
-        <p>Youtube</p>
-        <p>instagram</p>
+      <div className="space-y-4">
+        <h1 className="text-2xl font-bold">Follow Us</h1>
+        <p className="font-semibold text-gray-600">facebook</p>
+        <p className="font-semibold text-gray-600">Youtube</p>
+        <p className="font-semibold text-gray-600">instagram</p>
       </div>
     </div>
   );
