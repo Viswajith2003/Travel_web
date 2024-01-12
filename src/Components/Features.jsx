53e65d5c58d8482010a52a09627c3a1751891862
Viswajith2003 <viswajithviswa715@gmail.com>
2024-01-12 13:02:50 +0530

    home mobile few set

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -74,3 +74,89 @@ function Features() {
 }
 
 export default Features;
+
+
+
+
+
+
+
+// import React from "react";
+// import img from "../assets/features.jpg";
+// import { FaUserFriends } from "react-icons/fa";
+// import { AiFillContacts } from "react-icons/ai";
+// import { SiCashapp } from "react-icons/si";
+
+// function Features() {
+//   return (
+//     <div className="flex flex-col lg:flex-row items-center justify-center min-h-screen p-4 md:p-8">
+//       <div className="w-full md:w-2/3 lg:w-1/2 text-center">
+//         <div className="space-y-6">
+//           <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-5">
+//             Get Ready to Explore, Get <br />
+//             Ready to Experience!
+//           </h1>
+//           <p className="text-lg md:text-xl">
+//             Discover the Best Holiday Experiences with TripPixie! We're
+//             dedicated to curating the latest and greatest destinations from
+//             around the world, ensuring you always get the ultimate travel
+//             experience.
+//           </p>
+//         </div>
+//         <div className="w-full mt-10">
+//           <img
+//             className="rounded-xl hover:shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] mx-auto"
+//             src={img}
+//             alt="image"
+//           />
+//         </div>
+//       </div>
+
+//       <div className="w-full md:w-2/3 lg:w-1/2 space-y-14 pt-2 text-center">
+//         <div className="space-y-4 flex flex-col items-center">
+//           <div className="m-3">
+//             <FaUserFriends className="w-8 h-8" />
+//           </div>
+//           <div>
+//             <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-3">
+//               Friendly Service
+//             </h2>
+//             <p className="text-[#898888]">
+//               We will provide excellent and friendly service for the sake of our
+//               customers.
+//             </p>
+//           </div>
+//         </div>
+//         <div className="space-y-3 flex flex-col items-center">
+//           <div className="m-3">
+//             <AiFillContacts className="w-8 h-8" />
+//           </div>
+//           <div>
+//             <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-3">
+//               Unforgettable experience
+//             </h2>
+//             <p className="text-[#898888]">
+//               We will provide excellent and unforgettable experience for the
+//               sake of our customers.
+//             </p>
+//           </div>
+//         </div>
+//         <div className="space-y-3 flex flex-col items-center">
+//           <div className="m-3">
+//             <SiCashapp className="w-8 h-8" />
+//           </div>
+//           <div>
+//             <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-3">
+//               Affordable prices
+//             </h2>
+//             <p className="text-[#898888]">
+//               We will provide excellent and affordable prices for the sake of
+//               our customers.
+//             </p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+// export default Features;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,22 +5,22 @@ import { Link } from "react-scroll";
 
 function Home() {
   return (
-    <div className="flex justify-center items-center ml-10 mt-8">
-      <div className=" flex flex-col text-start justify-center gap-8 ml-20">
-        <h1 className="font-bold text-7xl leading-tight mb-">
+    <div className="flex flex-col lg:flex-row justify-center items-center ml-10">
+      <div className="flex flex-col text-center lg:text-start justify-center mt-20 mr-10 lg:mt-8 gap-8 lg:ml-20">
+        <h1 className="font-bold text-6xl lg:text-7xl leading-tight">
           Discover the Best Destinations
         </h1>
-        <p className="text-2xl ">
+        <p className="text-xl lg:text-2xl">
           With TripMood you can experience new travel and the best tourist
           destinations that we have to offer.
         </p>
-        <div className="mt-8 text-center items-center">
+        <div className="lg:mt-8 text-center items-center">
           <button className="bg-yellow-300 hover:bg-cyan-300 w-48 h-12 text-xl rounded-full font-bold">
             Destinations
           </button>
         </div>
       </div>
-      <div className="mt-14 ml-5 lg:mt-0 lg:w-4/5 w-full bg-blue-300">
+      <div className="lg:mt-14 lg:ml-5 lg:w-4/5 mt-10 w-full bg-blue-300">
         <img src={img} alt="picture" />
       </div>
     </div>
